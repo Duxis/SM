8d3d787f30c3bd1cf79972e4e0cd613ecb8627e4
Duxis <gettingtoxic@gmail.com>
2016-05-20 18:31:01 -0700

    New Feed
    Feed php html css layout

---
+++ feed.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style >
+      .wrapper {
+        color: red;
+  background-color: darkcyan;
+  width: 20vw;
+  height: 100vh;
+  position: absolute;
+}
+      
+      p {       width: 100%;
+    text-align: center;
+    font-size: 2.5em;
+    color: ghostwhite;
+    margin-top: 50px;
+    padding-top: 50px;}
+    </style>
+  </head>
+  <body>
+    <div class="wrapper" > <p> Sniddle Feed </p>
+
+
+
+    </div>
+
+  </body>
+</html>
